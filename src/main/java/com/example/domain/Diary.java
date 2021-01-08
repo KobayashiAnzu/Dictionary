9d7d30dc1655a783f748e8a4b422534a53967865
Taro <taro@yamada.com>
2021-01-08 15:16:03 +0900

    domainクラス

---
+++ src/main/java/com/example/domain/Diary.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Diary {
+
+	private Integer id;
+	private String contents;
+	private Date created;
+
+}
---
+++ src/main/java/com/example/domain/Korean.java
@@ -0,0 +1,16 @@
+package com.example.domain;
+
+import lombok.Data;
+
+@Data
+public class Korean {
+
+	private Integer id;
+	private String name;
+	private String reading;
+	private String mean;
+	private String category;
+
+
+
+}
